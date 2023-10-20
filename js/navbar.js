8c3bf9c4dee84c0153d3e075f788d4b37dc0f306
NealKneel <nealmancilla@gmail.com>
2023-10-20 23:40:42 +0800

    UpdatedNavbar/Sidebar (Functional)

--- js/navbar.js
+++ js/navbar.js
@@ -1,17 +1,28 @@
 //SHOWS AND HIDES THE SIDEBAR
 
 const sidebar = document.querySelector('.sidebar')
-function showSidebar() {
-    sidebar.classList.add('show');
-}
+const burger = document.querySelector('.toggle')
+const logo = document.querySelector('.logo-container')
+let burgerOpen = false;
 
-function hideSidebar() {
-    sidebar.classList.remove('show');
-}
+burger.addEventListener('click', () => {
+
+    if (burgerOpen) {
+      logo.classList.remove('hide')
+      sidebar.classList.remove('show');
+      burgerOpen = false;
+
+    } else {
+      logo.classList.add('hide')
+      sidebar.classList.add('show');
+      burgerOpen = true;
+    }
+
+  });
 
 //NAVBAR CLICKED NAVIGATION
 
-const links = document.querySelectorAll('ul li');
+const links = document.querySelectorAll('ul li a');
 
 links.forEach(link => {
     link.addEventListener('click', function(event) {
@@ -23,52 +34,51 @@ links.forEach(link => {
     });
 });
 
-//FUNCTION TO CHECK WHICH SECTION IS IN THE VIEW POINT
+// Function to check which section is in the viewpoint
 function checkActiveSection() {
-    const scrollPosition = window.scrollY + window.innerHeight / 2;
-    const buttons = document.querySelectorAll(".nav-buttons[href]");
-    
-    buttons.forEach(function(button) {
-      const targetId = button.getAttribute("href");
-      const targetSection = document.querySelector(targetId);
-      
-      if (targetSection) {
-        const sectionTop = targetSection.offsetTop;
-        const sectionBottom = sectionTop + targetSection.offsetHeight;
-  
-        if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
-          buttons.forEach(function(btn) {
-            btn.classList.remove("active");
-          });
-          button.classList.add("active");
-        }
-      }
-    });
-  }
-  
-  // ADD A SCROLL EVENT LISTENER TO CHECK THE ACTIVE SECTION ON SCROLL
-  window.addEventListener("scroll", checkActiveSection);
-  
-  // FUNCTION TO HANDLE BUTTON CLICK AND SCROLLING
-  function scrollToSection(event) {
-    event.preventDefault();
-    const targetId = this.getAttribute("href");
+  const scrollPosition = window.scrollY + window.innerHeight / 2;
+  const buttons = document.querySelectorAll(".nav-link[href]");
+
+  buttons.forEach(function (button) {
+    const targetId = button.getAttribute("href");
     const targetSection = document.querySelector(targetId);
-  
+
     if (targetSection) {
-      window.scrollTo({
-        top: targetSection.offsetTop,
-        behavior: "smooth"
-      });
-  
-      document.querySelector(".nav-buttons.active").classList.remove("active");
-      this.classList.add("active");
+      const sectionTop = targetSection.offsetTop;
+      const sectionBottom = sectionTop + targetSection.offsetHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
+        buttons.forEach(function (btn) {
+          btn.classList.remove("active");
+        });
+        button.classList.add("active");
+      }
     }
-  }
-  
-  // ADD CLICK EVENT LISTENERS TO NAVBAR BUTTONS
-  const buttons = document.querySelectorAll(".nav-buttons[href]");
-  buttons.forEach(function(button) {
-    button.addEventListener("click", scrollToSection);
   });
-  
+}
+
+// Add a scroll event listener to check the active section on scroll
+window.addEventListener("scroll", checkActiveSection);
+
+// Function to handle button click and scrolling
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute("href");
+  const targetSection = document.querySelector(targetId);
+
+  if (targetSection) {
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: "smooth",
+    });
+
+    document.querySelector(".nav-link.active").classList.remove("active");
+    this.classList.add("active");
+  }
+}
+
+// Add click event listeners to navbar buttons and sidebar buttons
+const buttons = document.querySelectorAll(".nav-link[href]");
+buttons.forEach(function (button) {
+  button.addEventListener("click", scrollToSection);
+});
