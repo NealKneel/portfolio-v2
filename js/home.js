60f1113b82342de608dd2cd72a0a1415dc8befa8
NealKneel <nealmancilla@gmail.com>
2023-10-24 00:23:18 +0800

    Home Section (Mobile Responsive) Stable

---
+++ js/home.js
@@ -0,0 +1,9 @@
+//////////////////////////TYPED.JS///////////////////////////////
+var typed = new Typed(".auto-type", {
+    strings: ["", "An aspiring <b>Web Developer</b>", "<b>Frontend</b> Web Developer", "<b>Backend</b> Web Developer"],
+    typeSpeed: 50,
+    backSpeed: 40,
+    loop: true
+})
+
+// document.addEventListener('DOMContentLoaded', function () {window.setTimeout(document.querySelector('svg').classList.add('animated'),5000);})
