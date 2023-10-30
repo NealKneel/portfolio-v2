f36b4ef396b17ba0fca091b671bca907e83d87e4
NealKneel <nealmancilla@gmail.com>
2023-10-30 22:25:54 +0800

    Added card carousel effect (not mobile responsive)

---
+++ .vscode/fiveserver.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    injectBody: true,
+    highlight: true,
+    navigate: false
+}
--- js/home.js
+++ js/home.js
@@ -7,3 +7,31 @@ var typed = new Typed(".auto-type", {
 })
 
 // document.addEventListener('DOMContentLoaded', function () {window.setTimeout(document.querySelector('svg').classList.add('animated'),5000);})
+
+//////////////////////////SWIPER.JS///////////////////////////////
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    grid: {
+      rows: 1,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+      breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+  });
